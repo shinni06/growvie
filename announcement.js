5e562d482d83bd134b0ce6eef142f5b22e31b732
Yu <yuschool101@gmail.com>
2026-01-07 23:05:17 +0800

    announcement php

--- announcement.js
+++ announcement.js
@@ -20,6 +20,12 @@ function postAnnouncement() {
         return;
     }
 
+    const monthNames = {
+        January: '01', February: '02', March: '03', April: '04',
+        May: '05', June: '06', July: '07', August: '08',
+        September: '09', October: '10', November: '11', December: '12'
+    }
+
     // Here you would typically save the announcement
     console.log('Announcement posted:', { day, month, year, title, content });
     
